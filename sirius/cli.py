ab04ed798b876999670eb9b63c34d8e6f7fd05da
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-12-15 01:38:37 +0530

    Progress

--- sirius/cli.py
+++ sirius/cli.py
@@ -5,10 +5,11 @@ import argparse
 import subprocess
 import configparser
 import socket
+import random
 
 IS_DEBUG = False
 def log(level, message):
-    if(IS_DEBUG or level.lower() == 'fatal' or level.lower() == 'out'):
+    if(IS_DEBUG or level.lower() in ['fatal', 'out', 'check', 'success']):
         print('[%s] %s'%(level, message))
 
 config = configparser.ConfigParser()
@@ -41,7 +42,7 @@ def resolveHostname(host):
     command = ['ping', param, '1', host]
     proc = subprocess.check_output(command)
 
-    ipv = find_between(str(proc), 'Reply from ', ': time<1ms')
+    ipv = find_between(str(proc), 'Reply from ', '%')
     log('trace', 'ip resolved address is %s'%(ipv,))
     log('trace', 'packet data: %s'%(proc,))
 
@@ -54,8 +55,9 @@ def c_verify_server_ping():
     except subprocess.CalledProcessError as e:
         log('fatal', "Host server offline or unavaliable. Try switching wifi channel using windows app NetSetMan or linux command nmcli")
         exit(-1)
-    log('out', "Host server online.. Starting CLI.")
-    
+
+    log('check', "host server online.")
+
 parser = argparse.ArgumentParser(prog='cli.py')
 parser.add_argument('--debug', action='store_true')
 
@@ -75,11 +77,60 @@ args = parser.parse_args(sys.argv[1:])
 IS_DEBUG = args.debug
 c_verify_server_ping()
 
+def hard_clone_section(cp, section_from, section_to):
+    items = cp.items(section_from)
+    cp.add_section(section_to)
+    for item in items:
+        cp.set(section_to, item[0], item[1])
+
+def rename_section(cp, section_from, section_to):
+    hard_clone_section(cp, section_from, section_to)
+    cp.remove_section(section_from)
+
+def expose(tcps, udps):
+    # Write configs
+    config = configparser.ConfigParser()
+    config.read('t_configs/template_client.ini')
+
+    config.set('common', 'server_addr', SERVER_HOST)
+    config.set('common', 'server_port', SERVER_PORT)
+    config.set('common', 'token', SERVER_TOKEN)
+
+    port = random.randint(8000,65535)
+    log('success', 'Your pairing port is %s'%(port,))
+    nl = 'link_'+str(port)
+    rename_section(config, 'link_', nl)
+
+    if(tcps is not None):
+        ns = 'p2p_tcp_'+str(port)
+        rename_section(config, 'p2p_tcp_', ns)
+        for tcp in tcps:
+            fns = ns+'_'+str(tcp)
+            hard_clone_section(config, ns, fns)
+            config.set(fns, 'local_port', str(tcp))
+        config.remove_section(ns)
+    else:
+        config.remove_section('p2p_tcp_')
+
+    if(udps is not None):
+        nu = 'secret_udp_'+str(port)
+        rename_section(config, 'secret_udp_', nu)
+        for udp in udps:
+            fns = nu+'_'+str(udp)
+            hard_clone_section(config, nu, fns)
+            config.set(fns, 'local_port', str(udp))
+        config.remove_section(nu)
+    else:
+        config.remove_section('secret_udp_')
+
+    log('trace', config.sections())
+    with open('t_configs/generated_client.ini', 'w') as configfile:    # save
+        config.write(configfile)
+    log('success', 'server config created.')
+
 if(args.cmd == 'expose'):
     # EXPOSE COMMANDS
-    if(args.tcp is not None):
-        for p in args.tcp:
-            print(p)
+    expose(args.tcp, args.udp)
 elif(args.cmd == 'connect'):
     # connect COMMANDS
     pass
